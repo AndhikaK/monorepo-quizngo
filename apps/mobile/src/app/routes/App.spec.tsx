efa4ed531f9ec55c3297f5b9f080b4d04a017c82
Andhika Kurniawan <andhika.kurniawan@adira.co.id>
2024-12-29 21:41:32 +0700

    test(mobile): fix unit test

--- apps/mobile/src/app/routes/App.spec.tsx
+++ apps/mobile/src/app/routes/App.spec.tsx
@@ -1,9 +1,11 @@
-import * as React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 
 import { DashboardScreen } from './dashboard';
 
 test('renders correctly', () => {
-  const { getByTestId } = render(<DashboardScreen />);
-  expect(getByTestId('heading')).toHaveTextContent('Welcome');
+  render(<DashboardScreen />);
+
+  const text = screen.getByText('This is dashboard screen');
+
+  expect(text).toBeOnTheScreen();
 });
