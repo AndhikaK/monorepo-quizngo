6e170d0b2651aecea2467be154a7f3f23e615863
Andhika Kurniawan <andhika.kurniawan@adira.co.id>
2025-01-11 23:28:58 +0700

    feat(server): setup EnvModule with validation using zod

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -7,12 +7,17 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { EnvService } from './config/env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(EnvService);
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const port = configService.get('PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
